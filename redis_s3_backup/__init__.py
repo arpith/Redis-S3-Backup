7f03669886d195fde22eb3d0b121021f154a2a0a
Arpith Siromoney <arpith@feedreader.co>
2016-07-04 16:31:52 -0400

    remove redundant main function (moved to command_line.py)

--- redis_s3_backup/__init__.py
+++ redis_s3_backup/__init__.py
@@ -19,14 +19,3 @@ def backup(redis_dir, bucket_name):
     rdb = open(file_path, 'r')
     key = time() + '.rdb'
     boto_s3.Bucket(bucket_name).put_object(Key=key, Body=rdb)
-
-def main():
-    """Parse command-line arguments and pass them to backup()"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--directory", help="Directory in which dump.rdb is located")
-    parser.add_argument("--bucket", help="S3 bucket to be used to store backups")
-    args = parser.parse_args()
-    backup(args.directory, args.bucket)
-
-if __name__ == '__main__':
-    main()
